e781a4c024b7c0123ba392cfcbbc85193001981a
Alan Martín <aespanah@est.ups.edu.ec>
2026-02-08 16:10:17 -0500

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const app = require('./app');
 require('./config/db.js');
 
-app.listen(app.get('port'), () => {
-  console.log('Servidor corriendo en puerto', app.get('port'));
-});
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});;
