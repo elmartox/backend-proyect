e2347324904bc065c21f70acbc26ec4a0e3afd48
Alan Martín <aespanah@est.ups.edu.ec>
2026-02-08 16:08:30 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 // URI de MongoDB Atlas
 const uri =
-  'mongodb+srv://user_proSE:MZZO3qQdUKfzQ4Ep@cluster0.7ft1zo2.mongodb.net/safeair?retryWrites=true&w=majority';
 
 mongoose.connect(uri)
   .then(() => console.log('MongoDB Atlas conectado'))
